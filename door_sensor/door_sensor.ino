46e7e3b4f2a6eb38fd0a005c7650b87979026dda
Jeff Rule <rule@tinygreenfrog.com>
2024-01-01 18:28:02 -0800

    WIP: added deep sleep mode

--- door_sensor/door_sensor.ino
+++ door_sensor/door_sensor.ino
@@ -13,105 +13,87 @@
 const char ssid[] = "LAK-FOXSPARROW";
 const char pass[] = "4793263208";
 
+// the keys to send to MQTT server
+const char mqtt_key[]="/door/pantry";
+const char mqtt_open[]="open";
+const char mqtt_closed[]="closed";
+
 WiFiClientSecure net;
 MQTTClient client;
 
-
 #define DOOR_SENSOR 15
 #define DOOR_OPEN 1
-#define DOOR_CLOSED 0
-#define READ_DELAY_MS 500
 
-int next_read=0;
-int open_close;
-
-unsigned long lastMillis = 0;
-
-
+boolean door_is_open;															// currently read door state
+RTC_DATA_ATTR boolean last_door_is_open=false;		// what was the last known state of the door, retained during deep sleep mode
 
 void connect() {
+	// connect to the wifi
   Serial.print("checking wifi");
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
     delay(1000);
   }
 
+	// connect to the MQTT
   Serial.print("\nconnecting MQTT");
   net.setInsecure();
   while (!client.connect("61850993d7e340d7b170bcd12078ef88.s2.eu.hivemq.cloud", "gyes51y767p", "@Mm5648970")) {
     Serial.print(".");
     delay(1000);
   }
-
   Serial.println("\nconnected!");
-
-  // client.subscribe("/hello");
-  // client.unsubscribe("/hello");
 }
 
-void messageReceived(String &topic, String &payload) {
-  Serial.println("incoming: " + topic + " - " + payload);
-
-  // Note: Do not use the client in the callback to publish, subscribe or
-  // unsubscribe as it may cause deadlocks when other things arrive while
-  // sending and receiving acknowledgments. Instead, change a global variable,
-  // or push to a queue and handle it in the loop after calling `client.loop()`.
-}
-
-void print_door_state(int door_state) {
-    if (door_state == DOOR_OPEN) {
+void print_door_state(boolean door_is_open) {
+    if (door_is_open == true) {
       Serial.println("Door is open!");
     } else {
       Serial.println("Door is closed");
     }
 }
-void send_door_state(int door_state) {
-    if (door_state == DOOR_OPEN) {
-      client.publish("/door1", "1");
+
+void send_door_state(int door_is_open) {
+    if (door_is_open == true) {
+      client.publish(mqtt_key, mqtt_open);
     } else {
-      client.publish("/door1", "0");
+      client.publish(mqtt_key, mqtt_closed);
     }
 }
 
+void mqtt_connect_and_send(boolean door_is_open) {
 
+	WiFi.begin(ssid, pass);
+  client.begin("61850993d7e340d7b170bcd12078ef88.s2.eu.hivemq.cloud", 8883, net);
+  connect();
+	client.loop();
+  delay(10);  // <- fixes some issues with WiFi stability
+  send_door_state(door_is_open);
+	client.loop();									// not sure if this is needed after the publish, but better safe then sorry
+
+}
 
 void setup() {
   Serial.begin(115200);
   while (!Serial) {}
 
   pinMode(DOOR_SENSOR, INPUT_PULLUP);
-  open_close=digitalRead(DOOR_SENSOR);
-  Serial.print("Initial Door State is: ");
-  print_door_state(open_close);
+  door_is_open=digitalRead(DOOR_SENSOR)==DOOR_OPEN;
+	print_door_state(door_is_open);
 
-  WiFi.begin(ssid, pass);
+	if (last_door_is_open != door_is_open) {
+		Serial.println("Door status changed, sending new state");
+		mqtt_connect_and_send(door_is_open);
+		last_door_is_open=door_is_open;
+	}
 
-  // Note: Local domain names (e.g. "Computer.local" on OSX) are not supported
-  // by Arduino. You need to set the IP address directly.
-  client.begin("61850993d7e340d7b170bcd12078ef88.s2.eu.hivemq.cloud", 8883, net);
-  client.onMessage(messageReceived);
+	Serial.println("Going To Sleep");
+	esp_deep_sleep_start();
 
-  connect();
-  Serial.println("Setup Done");
 }
 
 void loop() {
-  client.loop();
-  delay(10);  // <- fixes some issues with WiFi stability
 
-  if (!client.connected()) {
-    connect();
-  }
-  if (millis() > next_read) {
-    int pin_mode = digitalRead(DOOR_SENSOR); 
-  
-    if (pin_mode != open_close) {
-        print_door_state(pin_mode);
-        send_door_state(pin_mode);
-        open_close=pin_mode;
-        next_read = millis() + READ_DELAY_MS;
-    }
-  }
 }
 
 
